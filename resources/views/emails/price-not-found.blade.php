35483935a20bfa86e58e3d9ccefd1ab000bcb3a2
Cristian Quiroz <cristian@176lab.com>
2024-08-16 19:39:35 -0400

    add email price not found

---
+++ resources/views/emails/price-not-found.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Raga AI Email</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            font-size: 16px;
+        }
+    </style>
+</head>
+
+<body>
+<br>
+Estimado cliente, no tengo información para esa cotización
+<br>
+<br>
+<br>
+Raga AI
+</body>
+
+</html>
