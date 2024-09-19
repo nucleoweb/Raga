27a3ed94a8795afad0a4ebba43bea166f8b6a434
Cristian Quiroz <cristian@176lab.com>
2024-09-19 11:53:16 -0300

    fix save prompt bug

--- app/Livewire/Config/ConfigIndex.php
+++ app/Livewire/Config/ConfigIndex.php
@@ -14,37 +14,13 @@ class ConfigIndex extends Component {
 
         if ($config) {
             $this->prompt = $config->prompt;
+            $this->promptResponse = $config->departure_prompt;
         }
-
-        $this->promptResponse = $config->departure_prompt;
     }
 
     public function save() {
         $this->validate([
             'prompt' => 'required',
-        ]);
-
-        try {
-            // Check if a record exists
-            $config = Config::first();
-
-            if ($config) {
-                // Update the existing record
-                $config->update(['prompt' => $this->prompt]);
-            } else {
-                // Insert a new record
-                Config::create(['prompt' => $this->prompt]);
-            }
-
-            session()->flash('message', 'Prompt query guardado con éxito.');
-            return redirect()->to(request()->header('Referer'));
-        } catch (\Exception $e) {
-            session()->flash('error', $e->getMessage());
-        }
-    }
-
-    public function savePromtpResponse() {
-        $this->validate([
             'promptResponse' => 'required',
         ]);
 
@@ -53,19 +29,21 @@ class ConfigIndex extends Component {
 
             if ($config) {
                 // Update the existing record
+                $config->update(['prompt' => $this->prompt]);
                 $config->update(['departure_prompt' => $this->promptResponse]);
             } else {
                 // Insert a new record
+                Config::create(['prompt' => $this->prompt]);
                 Config::create(['departure_prompt' => $this->promptResponse]);
             }
 
-            session()->flash('message', 'Prompt respuesta guardado con éxito.');
+            session()->flash('message', 'Prompts guardados con éxito.');
+
             return redirect()->to(request()->header('Referer'));
         } catch (\Exception $e) {
             session()->flash('error', $e->getMessage());
         }
     }
-
     public function render() {
         return view('livewire.config.config-index')->layout('layouts.app');
     }
